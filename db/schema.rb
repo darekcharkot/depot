ab3a1c634873197c15287d8b29529dcb31283063
Darek Charkot <darek.charkot@gmail.com>
2016-02-29 18:54:09 +0100

    Add table Cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215224234) do
+ActiveRecord::Schema.define(version: 20160229172720) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
